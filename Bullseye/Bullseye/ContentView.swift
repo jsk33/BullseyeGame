543df7f79863dddf6d2754b56b24ed1c5b1b9f79
Jason Kim <jason33kim@gmail.com>
2019-12-27 13:02:46 -0500

    keep track of round

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @State var sliderValue = 50.0
     @State var target = Int.random(in: 1...100)
     @State var score = 0
+    @State var round = 1
         
     var body: some View {
         VStack {
@@ -51,6 +52,7 @@ struct ContentView: View {
                         dismissButton: .default(Text("awesome")) {
                             self.totalPoints()
                             self.target = Int.random(in: 1...100)
+                            self.round += 1
                             })
             }
             
@@ -66,7 +68,7 @@ struct ContentView: View {
                 Text("\(score)")
                 Spacer()
                 Text("Round:")
-                Text("999")
+                Text("\(round)")
                 Spacer()
                 Button(action: {}) {
                     Text("Info")
